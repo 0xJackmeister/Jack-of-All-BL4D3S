2891cc87112a8d8e0b8eddc528abe7a5241d6711
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2022-08-12 20:35:34 +0800

    Create ip-scanner.sh

---
+++ ip-scanner.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No 3 part network given , i.e. 113.22.35"
+else
+    ipAddress=$1
+
+    for i in {1..255} ;do 
+    (
+        {
+        ping -w 5 $ipAddress.$i ; 
+
+ #store last run success result(0) 
+        result=$(echo $?);
+
+ #clear all command output
+        } &> /dev/null
+
+
+#check if ping successful
+        if [ $result = 0 ]; then
+            echo  $ipAddress.$i : UP
+        else
+            echo  $ipAddress.$i : DOWN
+        fi &);
+    done
+
+fi
