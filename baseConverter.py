8abc45e0a5570e838b7481665db6d59afbc69760
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2022-08-19 01:00:33 +0800

    Create baseConverter.py

---
+++ baseConverter.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+string = str(input("Enter the number : "))
+
+base = int(input("Enter the base : "))
+
+dec = int(string,base)
+
+print("\n"+string+"'s Base "+str(base)+" is "+str(dec)) 
+
