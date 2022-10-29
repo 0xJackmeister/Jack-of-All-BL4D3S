8ae6a82e8172b49dd659c0ff5895cd33a9e2d191
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2022-10-29 21:22:07 +0800

    Update rot13.sh

--- rot13.sh
+++ rot13.sh
@@ -7,7 +7,7 @@ echo "ASCII  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h
 echo "ROT13  N O P Q R S T U V W X Y Z A B C D E F G H I J K L M n o p q r s t u v w x y z a b c d e f g h i j k l m"
 echo "\n"
 
-echo -n "Enter Hash : "
+echo -n "Hash : "
 tr '[a-z][A-Z]' '[n-za-m][N-ZA-M]'
 
 
