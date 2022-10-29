c5c3c5d012177cebe38ae3200397a0d386208bf2
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2022-10-29 21:13:32 +0800

    Update rot13.sh

--- rot13.sh
+++ rot13.sh
@@ -1,3 +1,13 @@
 #/bin/bash
 
+#Usage : Use it with pipeline or run the file and parse the hash
+
+echo "\n"
+echo "ASCII  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z"
+echo "ROT13  N O P Q R S T U V W X Y Z A B C D E F G H I J K L M n o p q r s t u v w x y z a b c d e f g h i j k l m"
+echo "\n"
+
+echo -n "Enter Hash : "
 tr '[a-z][A-Z]' '[n-za-m][N-ZA-M]'
+
+
