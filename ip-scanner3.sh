6e260c68bd1bbba2777526bac6a2a30e115fc547
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2022-11-01 22:16:10 +0800

    Update ip-scanner3.sh

--- ip-scanner3.sh
+++ ip-scanner3.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+if [ -z "$1" ]
+  then
+    echo "No 3 part network given , i.e. 113.22.35"
+else
+    ipAddress=$1
+echo "ICMP Scan Result"
+    for i in {1..255} ;do 
+    (
+        {
+        ping -w 5 $ipAddress.$i;  
+
+ #store last run success result(0) 
+        result=$(echo $?);
+
+ #clear all command output
+        } &> /dev/null
+
+
+#check if ping successful
+        if [ $result = 0 ]; then
+            echo  $ipAddress.$i : UP
+        else
+            echo  $ipAddress.$i : DOWN
+        fi &);
+    done
+echo "SYN Scan Result"
+nmap -sn  ipAddress.0/24 -v -oG - 
+fi
+
+   
